547d9d9e0e4dee537a147a8f9cdcf2f4ceb9d9ec
nikita.smirnov <nikita.smirnov@exactprosystems.com>
2025-02-12 15:05:15 +0400

    Corrected after review * migrate to common-go logger * use common-go v0.3.0

--- go.mod
+++ go.mod
@@ -3,8 +3,7 @@ module github.com/th2-net/th2-lwdp-grpc-fetcher-go
 go 1.21.0
 
 require (
-	github.com/rs/zerolog v1.29.0
-	github.com/th2-net/th2-common-go v0.2.2-0.20250211155721-876fd59417bc
+	github.com/th2-net/th2-common-go v0.3.0
 	github.com/th2-net/th2-grpc-common-go v0.0.1
 	github.com/th2-net/th2-grpc-lw-data-provider-go v0.0.1
 	google.golang.org/protobuf v1.36.5
@@ -13,6 +12,7 @@ require (
 require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/rs/zerolog v1.29.0 // indirect
 	golang.org/x/net v0.32.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
--- pkg/fetcher/message_fetcher.go
+++ pkg/fetcher/message_fetcher.go
@@ -19,10 +19,9 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"time"
 
-	"github.com/rs/zerolog/log"
 	"github.com/th2-net/th2-common-go/pkg/grpc"
+	"github.com/th2-net/th2-common-go/pkg/log"
 	common "github.com/th2-net/th2-grpc-common-go"
 	lwdp "github.com/th2-net/th2-grpc-lw-data-provider-go"
 	"google.golang.org/protobuf/types/known/timestamppb"
@@ -34,7 +33,24 @@ const (
 	lwdpService          = "DataProviderService"
 )
 
-func GetLastGroupedMessage(router grpc.Router, book string, group string, alias string, direction common.Direction, format string, deadlineMs uint32) (*lwdp.MessageGroupResponse, error) {
+var (
+	logger = log.ForComponent("lwdp-fetcher")
+)
+
+type LwdpFetcher struct {
+	dpClient lwdp.DataProviderClient
+}
+
+func NewLwdpFetcher(router grpc.Router) (*LwdpFetcher, error) {
+	conn, err := router.GetConnection(lwdpService)
+	if err != nil {
+		return nil, fmt.Errorf("getting connection for '%s' service failure: %w", lwdpService, err)
+	}
+	client := lwdp.NewDataProviderClient(conn)
+	return &LwdpFetcher{dpClient: client}, nil
+}
+
+func (f *LwdpFetcher) GetLastGroupedMessage(ctx context.Context, book string, group string, alias string, direction common.Direction, format string) (*lwdp.MessageGroupResponse, error) {
 	if book == "" {
 		return nil, errors.New("book can't be empty")
 	}
@@ -47,18 +63,6 @@ func GetLastGroupedMessage(router grpc.Router, book string, group string, alias 
 	if format != LwdpBase64Format && format != LwdpJsonParsedFormat {
 		return nil, fmt.Errorf("unknown format '%s'. known values ['%s','%s']", format, LwdpBase64Format, LwdpJsonParsedFormat)
 	}
-	if deadlineMs == 0 {
-		return nil, errors.New("deadline can't be 0")
-	}
-
-	conn, err := router.GetConnection(lwdpService)
-	if err != nil {
-		return nil, fmt.Errorf("getting the last message of '%s.%s.%s' book/group/alias failure: %w", book, group, alias, err)
-	}
-
-	client := lwdp.NewDataProviderClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(deadlineMs)*time.Millisecond)
-	defer cancel()
 
 	req := &lwdp.MessageGroupsSearchRequest{
 		StartTimestamp:  timestamppb.Now(),
@@ -69,24 +73,24 @@ func GetLastGroupedMessage(router grpc.Router, book string, group string, alias 
 		SearchDirection: lwdp.TimeRelation_PREVIOUS,
 		ResponseFormats: []string{format},
 	}
-	stream, err := client.SearchMessageGroups(ctx, req)
+	stream, err := f.dpClient.SearchMessageGroups(ctx, req)
 	if err != nil {
-		return nil, fmt.Errorf("searching message groups of '%s.%s.%s' book/group/alias failure: %w", book, group, alias, err)
+		return nil, fmt.Errorf("searching message groups by [book=%s, group=%s, alias=%s, direction=%d] parameters failure: %w", book, group, alias, direction, err)
 	}
 	for {
 		res, err := stream.Recv()
 		switch err {
 		case nil:
-			log.Trace().Str("operation", "GetLastGroupedMessage").Any("data", res.Data).Str("book", book).Str("group", group).Str("alias", alias).Any("direction", direction).Msg("received data")
+			logger.Trace().Str("operation", "GetLastGroupedMessage").Any("data", res.Data).Str("book", book).Str("group", group).Str("alias", alias).Any("direction", direction).Msg("received data")
 		case io.EOF:
-			log.Trace().Str("operation", "GetLastGroupedMessage").Str("book", book).Str("group", group).Str("alias", alias).Any("direction", direction).Msg("end of stream")
+			logger.Trace().Str("operation", "GetLastGroupedMessage").Str("book", book).Str("group", group).Str("alias", alias).Any("direction", direction).Msg("end of stream")
 			return nil, nil
 		default:
-			return nil, fmt.Errorf("receiving message groups of '%s.%s.%s' book/group/alias failure: %w", book, group, alias, err)
+			return nil, fmt.Errorf("receiving message groups by [book=%s, group=%s, alias=%s, direction=%d] parameters failure: %w", book, group, alias, direction, err)
 		}
 		msg := res.GetMessage()
 		if msg != nil {
-			log.Info().Str("operation", "GetLastGroupedMessage").Any("message-id", msg.MessageId).Msg("received message")
+			logger.Info().Str("operation", "GetLastGroupedMessage").Any("message-id", msg.MessageId).Msg("received message")
 			return res.GetMessage(), nil
 		}
 	}
